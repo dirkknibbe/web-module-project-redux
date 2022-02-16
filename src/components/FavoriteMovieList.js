52d264173dc2c08783b5a10dc611edc4c8ef83f5
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-15 20:15:49 -0700

    ok i finished

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
-
+import { removeFavorite } from "./../actions/favoritesActions";
 
 const FavoriteMovieList = (props) => {
-    const favorites = [];
-    
-    return (<div className="col-xs savedContainer">
-        <h5>Favorite Movies</h5>
-        {
-            favorites.map(movie=>{
-                return <div key={movie.id}>
-                    <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
-                        {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
-                    </Link> 
-                </div>
-            })
-        }
-    </div>);
-}
+  const { favorites, removeFavorite } = props;
 
+  const handleClick = (id) => {
+    removeFavorite(id);
+  };
 
-export default FavoriteMovieList;
+  return (
+    <div className="col-xs savedContainer">
+      <h5>Favorite Movies</h5>
+      {favorites.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <Link
+              className="btn btn-light savedButton"
+              to={`/movies/${movie.id}`}
+            >
+              {movie.title}
+              <span>
+                <span
+                  onClick={() => {
+                    handleClick(movie.id);
+                  }}
+                  class="material-icons"
+                >
+                  remove_circle
+                </span>
+              </span>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favoritesReducer.favorites,
+  };
+};
+
+export default connect(mapStateToProps, { removeFavorite })(FavoriteMovieList);
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
 
-const MovieList = (props)=> {
-    const movies = [];
+import { connect } from "react-redux";
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+import * as movieActions from "./../actions/movieActions";
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+const MovieList = (props) => {
+  const movies = props.movies;
 
-export default MovieList;
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
+
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movieReducer.movies,
+  };
+};
+
+export default connect(mapStateToProps)(MovieList);
